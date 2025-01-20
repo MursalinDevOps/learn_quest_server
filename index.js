cf7f6f8fdd836968afe1a61da9dc62cff864bf85
MursalinDevOps <mursalin1768@gmail.com>
2025-01-20 21:48:46 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+// todo: add env's to vercel
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -27,9 +29,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
